30f31d010f6e98befcf1ed6e8dad32d6a11415da
Andrey <wanted1345@gmail.com>
2022-04-07 19:01:34 +0900

    rollback auth form, move get token to join method

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -1,43 +1,87 @@
 <template>
   <v-container>
-    <v-row justify="center" align-content="center">
-      <v-text-field
-        v-model="room"
-        filled
-        label="Current chat room"
-        clearable></v-text-field>
-      <v-btn
-          v-on:click="join"
-          :disabled = "!logged">
-        Join chat
-      </v-btn>
-      <v-btn
-          v-on:click="ping"
-          :disabled = "!logged">
-        {{pingPongButton}}
-      </v-btn>
-    </v-row>
-    <v-row v-for="item in items" :key="item.id">
-      {{item.body}}
-    </v-row>
-    <v-row>
-      <v-textarea
-          v-model="message"
-          name="input-7-1"
-          filled
-          label="Text of message"
-          auto-grow
-          value=""
-      ></v-textarea>
-    </v-row>
-    <v-row>
-      <v-btn
-          color="primary"
-          :disabled = "!logged"
-          v-on:click="sendMsg">
-        Send message
-      </v-btn>
-    </v-row>
+
+      <v-row >
+        <v-text-field
+            v-model="login"
+            label="Login"
+            hide-details="auto"
+        ></v-text-field>
+      </v-row>
+      <v-row>
+        <v-text-field
+            v-model="password"
+            :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="passwordShow ? 'text' : 'password'"
+            name="input-10-2"
+            label="Password"
+            value=""
+            class="input-group--focused"
+            @click:append="passwordShow = !passwordShow"
+        ></v-text-field>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+              color="primary"
+              v-on:click="authorize">
+            Login
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+              color="error"
+              v-on:click="logout"
+              :disabled = "!logged">
+            Logout
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+              v-on:click="echo"
+              :disabled = "!logged">
+            Echo
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align-content="center">
+        <v-text-field
+            v-model="room"
+            filled
+            label="Current chat room"
+            clearable></v-text-field>
+        <v-btn
+            v-on:click="join"
+            :disabled = "!logged">
+          Join chat
+        </v-btn>
+        <v-btn
+            v-on:click="ping"
+            :disabled = "!logged">
+          {{pingPongButton}}
+        </v-btn>
+      </v-row>
+      <v-row v-for="item in items" :key="item.id">
+        {{item.text}}
+      </v-row>
+      <v-row>
+        <v-textarea
+            v-model="message"
+            name="input-7-1"
+            filled
+            label="Text of message"
+            auto-grow
+            value=""
+        ></v-textarea>
+      </v-row>
+      <v-row>
+        <v-btn
+            color="primary"
+            :disabled = "!logged"
+            v-on:click="sendMsg">
+          Send message
+        </v-btn>
+      </v-row>
   </v-container>
 </template>
 
@@ -75,9 +119,10 @@ export default {
           .receive("error", err => console.log("phoenix errored", err))
           .receive("timeout", () => console.log("timed out pushing"))
     },
-    join() {
+    async join() {
 
       console.log("Connect via websocket");
+      this.token = await API.getToken();
       this.socket = new Socket(API.socketURL, {params: {token: this.token}});
       this.socket.connect();
 
@@ -95,7 +140,7 @@ export default {
           });
     },
     sendMsg() {
-      this.channel.push("new_msg", {id: this.counter, body: this.message})
+      this.channel.push("new_msg", {room_id: 5, sender_id: this.login, text: this.message})
           .receive("ok", payload => console.log("phoenix replied:", payload))
           .receive("error", err => console.log("phoenix errored", err))
       this.message = "";
@@ -106,7 +151,7 @@ export default {
         login: this.login,
         password: this.password
       };
-      API.auth(auth).then(() => API.getToken().then(resp => (this.token = resp.data)))
+      API.auth(auth)
                     .then(() => this.logged = true);
     },
     logout() {
