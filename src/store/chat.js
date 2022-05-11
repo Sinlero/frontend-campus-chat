7843926a666a8ea0bf5926bd513e880edbd35b5b
Sinlero <wanted1345@gmail.com>
2022-05-11 09:33:48 +0900

    add chat state with dialogs and current dialogs (getters, actions, mutations)

--- src/store/chat.js
+++ src/store/chat.js
@@ -1,12 +1,53 @@
+import ChatApi from "@/chatApi/chatApi";
+
 export default {
     state: {
-        dialogs:[]
+        /*
+        dialogs: [
+            id: number,
+            name: string,
+            messages: [
+                {
+                    id: number,
+                    sender_id: number,
+                    text: string,
+                    time: timestamp
+                 }, ...
+            ],
+            users: []
+        ]
+         */
+        dialogs:[],
+        selectedDialog: null
     },
     getters: {
-        dialogs: (state) => state.dialogs
+        dialogs: (state) => state.dialogs,
+        currentDialog: (state) => state.selectedDialog,
+        lastMessage: (state) => (chatId) => {
+            const dialog = state.dialogs.find(dialog => dialog.id === chatId);
+            dialog.messages.sort(function (first, second) {
+                if (first.id > second.id) {
+                    return 1;
+                }
+                return -1;
+            })
+            return dialog.messages[dialog.messages.length - 1];
+        }
     },
     actions: {
+        fetchDialogs({commit}) {
+            ChatApi.getRooms().then((resp) => commit("updateDialogs", resp));
+        },
+        selectDialog({commit}, dialog) {
+            commit("updateSelectedDialog", dialog);
+        }
     },
     mutations: {
+        updateDialogs(state, dialogs) {
+            state.dialogs = dialogs;
+        },
+        updateSelectedDialog(state, dialog) {
+            state.selectedDialog = dialog;
+        }
     },
 };
