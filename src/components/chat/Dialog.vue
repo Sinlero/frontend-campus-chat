ea90d94558c51429e96419ef80265f50e92cb2f7
Sinlero <wanted1345@gmail.com>
2022-05-11 09:35:30 +0900

    add rendering messages

--- src/components/chat/Dialog.vue
+++ src/components/chat/Dialog.vue
@@ -1,27 +1,65 @@
 <template>
-<div>
-  <DialogMessage></DialogMessage>
-  <DialogMessage></DialogMessage>
-  <DialogMessage></DialogMessage>
-  <DialogMessage></DialogMessage>
-  <DialogMessage></DialogMessage>
-  <DialogMessage></DialogMessage>
-  <MessageInput/>
-</div>
+  <div>
+    <div class="messages" v-if="dialog !== null">
+      <DialogMessage
+          v-for="msg in dialog.messages"
+          :class="isSender(msg)"
+          :key="msg.id"
+          :id="msg.id"
+          :senderId="msg.sender_id"
+          :text="msg.text"
+          :time="msg.time"
+      ></DialogMessage>
+    </div>
+    <MessageInput/>
+  </div>
 </template>
 
 <script>
 import MessageInput from "@/components/chat/MessageInput";
 import DialogMessage from "@/components/chat/DialogMessage";
+import {useStore} from "vuex";
+import {computed} from "vue";
+
 export default {
   name: "DialogComponent",
   components: {
     DialogMessage,
     MessageInput
+  },
+  setup() {
+    const state = useStore();
+
+    const isSender = (msg) => {
+      if (msg.sender_id === state.getters.user.id) {
+        return {sender: true};
+      }
+      return {chat_member: true};
+    }
+
+    return {
+      dialog: computed(() => state.getters.currentDialog),
+      user: computed(() => state.getters.user),
+      isSender
+    }
   }
 }
 </script>
 
 <style scoped>
+.messages {
+  display: grid;
+}
 
+.sender {
+  display: flex;
+  color: teal;
+  justify-content: end;
+}
+
+.chat_member {
+  display: flex;
+  justify-content: start;
+  color: red;
+}
 </style>
