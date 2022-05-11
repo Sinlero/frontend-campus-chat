7c9772ff485000c5bca9f59db32e48ffb3d7219b
Sinlero <wanted1345@gmail.com>
2022-05-11 09:35:43 +0900

    add chats rendering

--- src/components/chat/ChatList.vue
+++ src/components/chat/ChatList.vue
@@ -1,21 +1,51 @@
 <template>
 <div>
-  <ChatItem name="Chat Room" preview-msg="Hello ALL !!!!!"></ChatItem>
-  <ChatItem name="Chat Room" preview-msg="Hello ALL !!!!!"></ChatItem>
-  <ChatItem name="Chat Room" preview-msg="Hello ALL !!!!!"></ChatItem>
-  <ChatItem name="Chat Room" preview-msg="Hello ALL !!!!!"></ChatItem>
-  <ChatItem name="Chat Room" preview-msg="Hello ALL !!!!!"></ChatItem>
+    <ChatItem class="chat_item"
+              v-for="dialog in dialogs"
+              @click="selectDialog(dialog)"
+              :key="dialog.id"
+              :name="dialog.name"
+              :preview-msg="getLastMessage(dialog.id).text"></ChatItem>
 </div>
 </template>
 
 <script>
 import ChatItem from "@/components/chat/ChatItem";
+import {useStore} from "vuex";
+import {computed, onMounted} from "vue";
 export default {
   name: "ChatList",
-  components: {ChatItem}
+  components: {ChatItem},
+  setup() {
+    const store = useStore();
+
+    const getLastMessage = (chatId) => {
+      return store.getters.lastMessage(chatId);
+    }
+
+    const selectDialog = (dialog) => {
+      return store.dispatch("selectDialog", dialog);
+    }
+
+    onMounted(() => store.dispatch("fetchDialogs"))
+
+    return {
+      dialogs: computed(() => store.getters.dialogs),
+      getLastMessage,
+      selectDialog
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.chat_item {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: solid darkgrey;
+}
+.chat_item:hover{
+  cursor: pointer;
+  background: gainsboro;
+}
 </style>
